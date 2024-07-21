5428a46ded2c8b6901f2171ff3db1e72d642e1a5
Dinamiet <iceman.sm39@gmail.com>
2024-07-21 20:55:30 +0200

    Improvements

--- include/cli.h
+++ include/cli.h
@@ -10,14 +10,17 @@
 
 #include "ansi_console_codes.h"
 
+#include <stdarg.h>
 #include <stddef.h>
 #include <stdint.h>
+
+#define MAX_CMD_LINE_LENGTH 64
 
 typedef struct _CLI_ CLI;
 
 typedef void (*CLI_CommandFunction)(CLI* cli, size_t argc, char* argv[]);
 typedef size_t (*CLI_ReadFunction)(char* str, size_t max);
-typedef size_t (*CLI_WriteFunction)(char* str);
+typedef size_t (*CLI_WriteFunction)(char* format, va_list params);
 
 typedef struct _CLICommand_
 {
@@ -30,12 +33,17 @@ typedef struct _CLI_
 {
 	char*             Prompt;
 	CLICommand*       CommandList;
+	char              WorkingCommand[MAX_CMD_LINE_LENGTH + 1];
 	CLI_ReadFunction  Read;
 	CLI_WriteFunction Write;
 } CLI;
 
 void         CLI_Init(CLI* cli, char* prompt, CLICommand* cmdList, CLI_ReadFunction read, CLI_WriteFunction write);
-void         CLI_ProcessCommand(CLI* cli, char* commandLine);
+void         CLI_Process(CLI* cli);
+void         CLI_DoCommand(CLI* cli, char* command);
+size_t       CLI_Write(CLI* cli, char* format, ...);
+size_t       CLI_Read(CLI* cli, char* str, size_t max);
+
 void         CLI_Cmd(CLI* cli, int argc, char* argv[]);
 extern char* CLI_Help[];
 
